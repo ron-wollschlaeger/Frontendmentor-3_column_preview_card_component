e14ba9ad9c37da42285d9dae8fc87962a40c9656
ron-wollschlaeger <ron.wollschlaeger@frontshape.de>
2023-10-23 10:46:18 +0200

    Upload Challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./*.{html,js}"],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      'title': ['Big Shoulders Display', 'sans-serif'],
+      'body': ['Lexend Deca', 'sans-serif'],
+    },
     extend: {
       animation: {
         'spin-slow': 'spin 3s linear infinite',
